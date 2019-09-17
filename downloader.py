d8c8eb515b88f248aaab8ca3cf9c8cb58ad75b64
许智海 <18829040039@163.com>
2019-09-17 20:00:16 +0800

    抖音视频下载器

--- downloader.py
+++ downloader.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/9/17 14:25
-# @Author  : xuzhihai0723
+# @Author  : Esbiya
 # @Email   : 18829040039@163.com
 # @File    : downloader.py
 # @Software: PyCharm
